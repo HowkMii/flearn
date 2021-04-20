89138e9c1bdab2beaba9ada0f13c27daae8056a2
unknown <abdelhakim@univ-tiaret.dz>
2021-04-20 21:28:17 +0100

    widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatefulWidget{
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        
+
       ),
     );
   }
+  
   
   
  
