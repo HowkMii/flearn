b7e411350339c4960376da1f96a87f10d3f2c835
unknown <abdelhakim@univ-tiaret.dz>
2021-04-20 21:30:46 +0100

    widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
+        appBar:AppBar(title: Text("appbar title"),) ,
+        body: Text("app bar"),
+       
 
       ),
     );
