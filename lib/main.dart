f38f087f5a20795309fb0a2ed505c8699a21ddba
unknown <abdelhakim@univ-tiaret.dz>
2021-04-22 00:05:06 +0100

    Text Style decorationStyle

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _MyAppState extends State<MyApp>{
           ],
           decoration: TextDecoration.combine([TextDecoration.underline, TextDecoration.overline]),
           decorationColor: Colors.black12,
+          decorationStyle: TextDecorationStyle.dotted,
 
         ),)
           
