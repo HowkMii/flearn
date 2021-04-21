90967f9b310652f627ff6fdd0e4eb393ddcb88ac
unknown <abdelhakim@univ-tiaret.dz>
2021-04-21 23:01:37 +0100

    color fromARGB

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp>{
       home: Scaffold(
         appBar:AppBar(title: Text("appbar title"),) ,
         body: Container(
-          color: Color.fromRGBO(255, 255, 255, 0.4) ,
+          color: Color.fromARGB(a, r, g, b) ,
           
          
           child: Column(
