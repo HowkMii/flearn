c92d80f120114356727c50d16cbae4b4c74967a0
unknown <abdelhakim@univ-tiaret.dz>
2021-04-22 09:01:08 +0100

    Raised button RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -13,47 +13,30 @@ class MyApp extends StatefulWidget{
 
 
 class _MyAppState extends State<MyApp>{
-  int cntr = 0;
-  void _incrementConter(){
-    setState(() {
-      cntr+=3;
-    });
-    
-  }
+  var str="";
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         appBar:AppBar(title: Text("appbar title"),) ,
-        body: Text("Body title", style: TextStyle(
-          color: Colors.black,
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          wordSpacing: 40,
-          letterSpacing: 40,
-          height: 20,
-          shadows: <Shadow>[
+        body: Column(children: <Widget>[
+          Text(str,style: TextStyle( ),),
+          RaisedButton(onPressed: null)
+
+        ],)
+         
             
-            Shadow(
-              offset: Offset(10.0, 10.0),
-              blurRadius: 9,
-              color: Colors.black)
+            
 
-          ],
-          decoration: TextDecoration.combine([TextDecoration.underline, TextDecoration.overline]),
-          decorationColor: Colors.black12,
-          decorationStyle: TextDecorationStyle.dotted,
-          decorationThickness: 10,
-
-        ),)
+        )
           
         
          
         
         
-        ),
+        );
        
-        );  
+        
      
 
 
