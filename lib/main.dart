ffb15d24e5ef2ffcb3844007ae929d937ffb67df
unknown <abdelhakim@univ-tiaret.dz>
2021-05-08 05:09:32 +0100

    App bar

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class _MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+        home: Scaffold(appBar:AppBar(
+          backgroundColor: Colors.yellowAccent,
+        ) ,),
      
 
         
