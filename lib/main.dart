3adb5d5b027efd42dcfa46232781e47a056a8802
unknown <abdelhakim@univ-tiaret.dz>
2021-05-09 06:49:19 +0100

    CON

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,7 @@ class _MyAppState extends State<MyApp> {
        title: Text("Quiz"),
       ),
       body: Container(
-        width: double.infinity,
-        margin: EdgeInsets.all(10.0),
-        child: Column(children: <Widget>[
-          Text(_question[0], style: TextStyle(fontSize:30 ),textAlign: TextAlign.center,),
-
-
-        ],),
+       
 
       ),
       
