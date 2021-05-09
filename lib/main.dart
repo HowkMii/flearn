6ec4cda18e50ee20a40a16d222ac4ca11a003e5d
unknown <abdelhakim@univ-tiaret.dz>
2021-05-09 06:19:20 +0100

    add Stack

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,26 @@ class _MyHomePageState extends State<MyHomePage>{
           title: Text("Appbar", style: _textStyle,),
         ),
         body: Container(
+          alignment: AlignmentDirectional.center,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                width: 400,
+                height: 400,
+                color: Colors.blueAccent,
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.blueAccent,
+              ),
+               Container(
+                width: 100,
+                height: 100,
+                color: Colors.blueAccent,
+              )
+            ],
+          ),
          
         ),
       ),
