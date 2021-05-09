2fed0a98de17737f9c1c519d70c8fdcaa2398ae6
unknown <abdelhakim@univ-tiaret.dz>
2021-05-09 06:30:23 +0100

    statefullwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Container(
