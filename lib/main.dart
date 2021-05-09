c95b368a22d6ddbcb3548b3f8f283fb15f81cc4d
unknown <abdelhakim@univ-tiaret.dz>
2021-05-09 06:16:03 +0100

    Stack & Alignment

--- lib/main.dart
+++ lib/main.dart
@@ -42,21 +42,7 @@ class _MyHomePageState extends State<MyHomePage>{
           title: Text("Appbar", style: _textStyle,),
         ),
         body: Container(
-          margin: EdgeInsets.only(top: 23),
-          padding: EdgeInsets.only(top:23),
-          child: Row(
-           
-            children: <Widget>[
-               
-              Text("bb",style: TextStyle(color: Colors.black38,fontSize: 34),),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: 
-                MyText("aad", _textStyle),
-    
-              ),
-            ],
-          )
+         
         ),
       ),
 
