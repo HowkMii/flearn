934117847dea51250c03716ffe3e793b1244bce2
unknown <abdelhakim@univ-tiaret.dz>
2021-05-09 02:06:37 +0100

    margin 7

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage>{
           title: Text("Appbar", style: TextStyle(color: Colors.black),),
         ),
         body: Container(
-          margin: EdgeInsets.symmetric(vertical: 8.0,horizontal: 18.0),
+          margin: EdgeInsets.only(top: 23),
           child: Row(
            
             children: <Widget>[
