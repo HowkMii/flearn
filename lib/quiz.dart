f20be282e43617c6cd1d39ac920df95c5c7994d6
unknown <abdelhakim@univ-tiaret.dz>
2021-05-12 02:49:25 +0100

    update

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,18 +3,18 @@ import 'package:flutter/material.dart';
 
 import 'answer.dart';
 class Quiz extends StatelessWidget {
-  final answerQuestion;
-  final _questionIndex;
-  final _question;
-  Quiz(this._question,this._questionIndex,this.answerQuestion)
+  final Function answerQuestion;
+  final int  questionIndex;
+  final List<Map<String, Object>> question;
+  Quiz(this.question,this.questionIndex,this.answerQuestion);
   @override
   Widget build(BuildContext context) {
     
 
     return Column(
           children:<Widget> [
-            Question(_question[_questionIndex]['questionText']),
-            ...(_question[_questionIndex]['answer'] as List<String>).map((ansewr){
+            Question(question[questionIndex]['questionText']),
+            ...(question[questionIndex]['answer'] as List<String>).map((ansewr){
               return Ansewr(answerQuestion, ansewr);
 
             }).toList()
