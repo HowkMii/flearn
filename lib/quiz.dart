a22760d5d40bfbc71c65ca9ddce27efcf2b0e5e3
unknown <abdelhakim@univ-tiaret.dz>
2021-05-12 02:45:54 +0100

    quiz

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,9 +1,27 @@
+import 'package:flearn/question.dart';
 import 'package:flutter/material.dart';
+
+import 'answer.dart';
 class Quiz extends StatelessWidget {
+  final answerQuestion;
+  final _questionIndex;
+  final _question;
+  Quiz(this._question,this._questionIndex,this.answerQuestion)
   @override
   Widget build(BuildContext context) {
-    return Container(
+    
+
+    return Column(
+          children:<Widget> [
+            Question(_question[_questionIndex]['questionText']),
+            ...(_question[_questionIndex]['answer'] as List<String>).map((ansewr){
+              return Ansewr(answerQuestion, ansewr);
+
+            }).toList()
+            
+          ],
+        );
       
-    );
+    
   }
 }
