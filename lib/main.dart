1da6f6969aefbfc8493a3fc5a35a5048cc2b8efe
unknown <abdelhakim@univ-tiaret.dz>
2021-05-15 03:50:53 +0100

    dark mode design

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var w =Colors.white;
   int _questionIndex = 0;
   int _totalScore =0;
   int num0=0;
@@ -66,6 +67,7 @@ class _MyAppState extends State<MyApp> {
        title: Text("Quiz"),
       ),
       body: Container(
+        color: Colors.white,
         child:_questionIndex<_question.length
         ? Quiz(_question,_questionIndex,answerQuestion)
         : Result(_resetQuiz, _totalScore),
