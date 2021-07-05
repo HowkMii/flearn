285e81e392de2134ceaf7b0e3dfca51989df579b
unknown <abdelhakim@univ-tiaret.dz>
2021-07-05 20:30:43 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter  App',
+    home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('flutter App'),),
+      body: Container(
+
       ),
     );
   }
