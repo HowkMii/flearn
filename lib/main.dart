d2c17c3125e881ecbcce154dfae99e04d499122b
unknown <abdelhakim@univ-tiaret.dz>
2021-07-05 20:33:50 +0100

    text color

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+Color w = Colors.white;
+Color b = Colors.black;
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,7 +19,7 @@ class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text('flutter App'),),
+      appBar: AppBar(title: Text('flutter App',style: TextStyle(color: b),),),
       body: Container(
 
       ),
