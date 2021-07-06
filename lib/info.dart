5c1899ce61a482e577aed42028afddbd3248a023
unknown <abdelhakim@univ-tiaret.dz>
2021-07-06 10:23:52 +0100

    map function1

---
+++ lib/info.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+class Info{
+  String name;
+  int height;
+  DateTime dateTime;
+  Info({
+    @required this.name, this.height,this.dateTime
+  });
+}
