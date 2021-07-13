60b2133bfbe8669bd0bee97f85c7549833d45da3
unknown <abdelhakim@univ-tiaret.dz>
2021-07-13 17:12:29 +0100

    Passing data between Screens

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -1,20 +1,22 @@
-import 'package:flearn/screen2.dart';
+
 import 'package:flutter/material.dart';
 class ScreenOne extends StatelessWidget {
   
    void selectScreen(BuildContext ctx){
-    Navigator.of(ctx).pushNamed(
+    Navigator.of(ctx).pushReplacementNamed(
      '/x2',
     );
   }
   @override
   Widget build(BuildContext context) {
+    final routeArg = ModalRoute.of(context).settings.arguments as Map<String, Object>;
+    
     return Scaffold(
       appBar: AppBar(title: Text("screen one"),),
       body: Center(
         child:  Column(
           children: <Widget>[
-            Text("",style: TextStyle(fontSize: 30)),
+            Text("$routeArg['id']",style: TextStyle(fontSize: 30)),
             InkWell(
                   child: Text("Go to screen1",style: TextStyle(fontSize: 30)),
                   onTap: (){
