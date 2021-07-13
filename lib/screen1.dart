101e912888df45c97d463e35c56ede7d0231f0f2
unknown <abdelhakim@univ-tiaret.dz>
2021-07-13 17:13:51 +0100

    Passing data between Screens

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -17,6 +17,7 @@ class ScreenOne extends StatelessWidget {
         child:  Column(
           children: <Widget>[
             Text("$routeArg['id']",style: TextStyle(fontSize: 30)),
+            Text(routeArg['title'],style: TextStyle(fontSize: 30)),
             InkWell(
                   child: Text("Go to screen1",style: TextStyle(fontSize: 30)),
                   onTap: (){
