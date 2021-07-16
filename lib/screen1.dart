2be842538dce59652fb8daa8f023fefb26755dfd
unknown <abdelhakim@univ-tiaret.dz>
2021-07-16 14:45:32 +0100

    tabbar

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -1,4 +1,5 @@
 
+import 'package:flearn/screen2.dart';
 import 'package:flutter/material.dart';
 class ScreenOne extends StatelessWidget {
   
@@ -11,24 +12,38 @@ class ScreenOne extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArg = ModalRoute.of(context).settings.arguments as Map<String, Object>;
     
-    return Scaffold(
-      appBar: AppBar(title: Text("screen one"),),
-      body: Center(
-          
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            Text("${routeArg['id']}",style: TextStyle(fontSize: 30)),
-            Text(routeArg['title'],style: TextStyle(fontSize: 30)),
-            InkWell(
-                  child: Text("Go to screen1",style: TextStyle(fontSize: 30)),
-                  onTap: (){
-                    selectScreen(context);
-                  },
-                  ),
-          ],
-        ), 
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("screen one"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.category),text: ("Screen 1"),),
+              Tab(icon: Icon(Icons.category),text: ("Screen 1"),),
+            ],
+          ),
+          ),
+        body: TabBarView(
+            children: <Widget>[
+              ScreenTwo(),
+              ScreenTwo(),
+            ],
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              Text("${routeArg['id']}",style: TextStyle(fontSize: 30)),
+              Text(routeArg['title'],style: TextStyle(fontSize: 30)),
+              InkWell(
+                    child: Text("Go to screen1",style: TextStyle(fontSize: 30)),
+                    onTap: (){
+                      selectScreen(context);
+                    },
+                    ),
+            ],
+          ), 
+        ),
       ),
     );
   }
